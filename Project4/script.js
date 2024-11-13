8e46b176d24d4487ae3010bf452ea5ce8f4af563
sujal10022005 <144321575+sujal10022005@users.noreply.github.com>
2024-11-13 22:58:31 +0530

    update

--- Project4/script.js
+++ Project4/script.js
@@ -6,19 +6,7 @@ document.addEventListener("mousemove",function(dets){
         blur.style.left=dets.x-250+"px"
         blur.style.top=dets.y-250+"px"
 })
-var h4all=document.querySelectorAll("#nav h4")
-h4all.forEach(function(elem){
-        elem.addEventListener("mouseenter",function(){
-            crsr.style.scale=3
-            crsr.style.border="1px solid #fff"
-            crsr.style.backgroundColor="transparent"
-        })
-        elem.addEventListener("mouseleave",function(){
-            crsr.style.scale=1
-            crsr.style.border="0px solid #95C11E"
-            crsr.style.backgroundColor="#95C11E"
-        })
-})
+
 gsap.to("#nav",{
     backgroundColor:"#000",
     duration:.5,
@@ -37,19 +25,60 @@ gsap.to("#main",{
     scrollTrigger:{
         trigger:"#main",
         scroller:"body",
-        markers:true,
+        //markers:true,
         start:"top -25%",
         end:"top -75%",
         scrub:2
     }
 })
 gsap.from("#about-us img,#about-us-in",{
-    y:50,
+    y:90,
     opacity:0,
     duration:1,
     stagger:0.4,
     scrollTrigger:{
-        trigger:"about-us",
+        trigger:"#about-us",
         scroller:"body",
+        //markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:2
     }
 })
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    stagger:0.1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        //markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1
+    }
+})
+gsap.from("#colon1",{
+   y:-70,
+   x:-70,
+   scrollTrigger:{
+    trigger:"#colon1",
+    scroller:"body",
+    start:"top 55%",
+    end:"top 45%",
+    scrub:4
+   }  
+})
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+     trigger:"#colon1",
+     scroller:"body",
+     start:"top 55%",
+     end:"top 45%",
+     scrub:4
+    }  
+ })
+ gsap.from
